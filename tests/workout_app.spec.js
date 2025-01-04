b6fc815df8e0d38cea799c8bc91bfa6f55797cd3
jabss <jabrijuhinin93@gmail.com>
2025-01-04 17:49:30 +0400

    test for login form can be opened pass

--- tests/workout_app.spec.js
+++ tests/workout_app.spec.js
@@ -1,9 +1,18 @@
-const { test, expect } = require('@playwright/test')
+const { test, expect, describe } = require('@playwright/test')
 
-test('front page can be opened', async ({ page }) => {
-  await page.goto('http://localhost:5173')
+describe('Workout Tracker app', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  
+    const locator = await page.getByText('Let\'s do this')
+    await expect(locator).toBeVisible()
+    await expect(page.getByText('This is not a real online service! You know you need something like this in your life to help you realize your deepest dreams.')).toBeVisible()
+  })
 
-  const locator = await page.getByText('Let\'s do this')
-  await expect(locator).toBeVisible()
-  await expect(page.getByText('This is not a real online service! You know you need something like this in your life to help you realize your deepest dreams.')).toBeVisible()
+  test('login form can be opened', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    await page.getByRole('button', { name: 'Login', exact: true }).click()
+  })
 })
+
