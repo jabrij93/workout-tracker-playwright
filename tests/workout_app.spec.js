151dbefa1e8bd88de4976fb3071f2bea83452301
jabss <jabrijuhinin93@gmail.com>
2025-01-07 20:57:58 +0400

    group test as 'when logged in' and test add new workout pass

--- tests/workout_app.spec.js
+++ tests/workout_app.spec.js
@@ -1,4 +1,4 @@
-const { test, expect, describe } = require('@playwright/test')
+const { test, expect, describe, beforeEach } = require('@playwright/test')
 
 describe('Workout Tracker app', () => {
   test('front page can be opened', async ({ page }) => {
@@ -9,13 +9,21 @@ describe('Workout Tracker app', () => {
     await expect(page.getByText('This is not a real online service! You know you need something like this in your life to help you realize your deepest dreams.')).toBeVisible()
   })
 
-  test('login form can be opened', async ({ page }) => {
-    await page.goto('http://localhost:5173')
+  describe('when logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.goto('http://localhost:5173')
 
     await page.getByTestId('username').first().fill('mluukkai')
     await page.getByTestId('password').last().fill('salainen')
     await page.getByRole('button', { name: 'Login', exact: true }).click()
     await expect(page.getByText('Matti Luukkainen')).toBeVisible()
-  })
+    })
+
+    test('a new workout can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'NEW +', exact: true }).nth(1).click()
+      await page.getByTestId('workout').fill('a new workout created by playwright')
+      await page.getByRole('button', { name: 'Add Workout' }).click()
+    })
+  })  
 })
 
