5b96e1f045685b68e3e7f888f0d220f2eaaf8a81
jabss <jabrijuhinin93@gmail.com>
2025-01-13 00:41:48 +0400

    update test database to initialize with two users

--- tests/workout_app.spec.js
+++ tests/workout_app.spec.js
@@ -1,6 +1,28 @@
 const { test, expect, describe, beforeEach } = require('@playwright/test')
 
 describe('Workout Tracker app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3002/api/testing/reset')
+
+    await request.post('http://localhost:3002/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    });
+
+    await request.post('http://localhost:3002/api/users', {
+      data: {
+        name: 'Superuser',
+        username: 'root',
+        password: 'salainen'
+      }
+    });
+
+    await page.goto('http://localhost:5173')
+  })
+
   test('front page can be opened', async ({ page }) => {
     await page.goto('http://localhost:5173')
   
@@ -9,30 +31,28 @@ describe('Workout Tracker app', () => {
     await expect(page.getByText('This is not a real online service! You know you need something like this in your life to help you realize your deepest dreams.')).toBeVisible()
   })
 
-  describe('when logged in', () => {
-    beforeEach(async ({ page }) => {
-      await page.goto('http://localhost:5173')
-      await page.getByTestId('username').first().fill('mluukkai')
-      await page.getByTestId('password').last().fill('salainen')
-      await page.getByRole('button', { name: 'Login', exact: true }).click()
-      await expect(page.getByText('Matti Luukkainen')).toBeVisible()
-    })
+  // describe('when logged in', () => {
+  //   beforeEach(async ({ page }) => {
+  //     await page.goto('http://localhost:5173')
+  //     await page.getByTestId('username').first().fill('mluukkai')
+  //     await page.getByTestId('password').last().fill('salainen')
+  //     await page.getByRole('button', { name: 'Login', exact: true }).click()
+  //     await expect(page.getByText('Matti Luukkainen')).toBeVisible()
+  //   })
 
-    test('a new workout can be created', async ({ page }) => {
-      await page.getByRole('button', { name: 'NEW +', exact: true }).nth(1).click()
-      await page.getByTestId('workout').fill('pull-ups by playwright 15')
+  //   test('a new workout can be created', async ({ page }) => {
+  //     await page.getByRole('button', { name: 'NEW +', exact: true }).nth(1).click()
+  //     await page.getByTestId('workout').fill('pull-ups by playwright 19')
 
-      // Wait for the network request to complete after clicking "Add Workout"
-      await Promise.all([
-        page.waitForResponse(response => {
-          console.log('Response received:', response.url(), response.status());
-          return response.url().includes('/api/workouts') && response.status() === 201}),
-        page.getByRole('button', { name: 'Add Workout' }).click(),
-      ]);
-
-      await expect(page.getByText('pull-ups by playwright 15').first()).toBeVisible()
-
-    })
-  })  
+  //     // Wait for the network request to complete after clicking "Add Workout"
+  //     await Promise.all([
+  //       page.waitForResponse(response => {
+  //         console.log('Response received:', response.url(), response.status());
+  //         return response.url().includes('/api/workouts') && response.status() === 201}),
+  //       page.getByRole('button', { name: 'Add Workout' }).click(),
+  //     ]);
+  //     await expect(page.getByText('pull-ups by playwright 19').first()).toBeVisible()
+  //   })
+  // }) 
 })
 
