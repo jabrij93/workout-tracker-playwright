b5de06a8fd66363d0babdd1da644ae9758accb9b
jabss <jabrijuhinin93@gmail.com>
2025-01-22 01:21:56 +0400

    refactored add workout(without date) into helper.js(createWorkout)

--- tests/helper.js
+++ tests/helper.js
@@ -4,5 +4,11 @@ const loginWith = async (page, username, password)  => {
     await page.getByTestId('password').fill(password)
     await page.getByRole('button', { name: 'Login', exact: true }).click()
 }
+
+const createWorkout = async (page, content) => {
+    await page.getByRole('button', { name: 'NEW +', exact: true }).nth(1).click();
+    await page.getByTestId('workout').fill(content);
+    await page.getByRole('button', { name: 'Add Workout' }).click()
+}
   
-export { loginWith }
+export { loginWith, createWorkout }
